3aaecfb0e8bc9bfc03b528511ae7a9a05f9d9632
Louis Tan <louistan600@gmail.com>
2026-03-24 01:48:20 +0800

    add headers: Authorization

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -23,8 +23,10 @@ export const registerApi = async (email, password) => {
 }
 
 export const getTodoApi = async () => {
+    const token = localStorage.getItem('token')
     const response = await fetch("/api/Todo", {
         method: 'GET',
+        headers: { 'Authorization': `Bearer ${token}` }
     });
     if (!response.ok)
         throw new Error("Failed to fetch");
@@ -33,9 +35,13 @@ export const getTodoApi = async () => {
 }
 
 export const addTodoApi = async (todoDto) => {
+    const token = localStorage.getItem('token')
     const response = await fetch("/api/Todo", {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
         body: JSON.stringify(todoDto),
     });
     if (!response.ok)
@@ -45,9 +51,13 @@ export const addTodoApi = async (todoDto) => {
 }
 
 export const updateTodoApi = async (id, todoDto) => {
+    const token = localStorage.getItem('token')
     const response = await fetch(`/api/Todo/${id}`, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
         body: JSON.stringify(todoDto),
     });
     if (!response.ok)
@@ -57,8 +67,10 @@ export const updateTodoApi = async (id, todoDto) => {
 }
 
 export const deleteTodoApi = async (id) => {
+    const token = localStorage.getItem('token')
     const response = await fetch(`/api/Todo/${id}`, {
         method: 'DELETE',
+        headers: { 'Authorization': `Bearer ${token}` }
     });
     if (!response.ok)
         throw new Error("Failed to delete task");
