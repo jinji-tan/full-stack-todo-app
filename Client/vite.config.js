ba02c96d360cf99c6add95c62e6b112fed579973
Louis Tan <louistan600@gmail.com>
2026-03-26 02:08:15 +0800

    fix target:

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // your ASP.NET backend
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false, // allows self-signed certificate
+        secure: false,
       }
     }
   }
